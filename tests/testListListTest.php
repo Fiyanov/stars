f40aa8374e1a9896b8a30a3c91fd95d7d9adeb63
x49x46 <fiyanov@yandex.ru>
2018-03-02 18:11:16 +0600

    test update

--- tests/testListListTest.php
+++ tests/testListListTest.php
@@ -15,13 +15,30 @@ class ListTest extends \UnitTestCase
             is_array((new StarService())->getList()),
             'Array required!'
         );
+    }
 
+    public function testPersonCase()
+    {
         $result = (new StarService())->getPersona(1);
 
         $this->assertEquals(
             true,
-            is_array($result) || is_bool($result)  ,
-            'Result must be filled'
+            (is_array($result) && !empty($result)) || is_bool($result),
+            'Result must be filled array or boolean'
         );
+
+        if ($result) {
+            $this->assertCount(
+                1,
+                $result,
+                'Result must have only one item'
+            );
+        } else {
+            $this->assertEquals(
+                false,
+                $result,
+                'Result must be "false" if nothing to find'
+            );
+        }
     }
 }
