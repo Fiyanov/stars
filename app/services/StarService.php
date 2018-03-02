f07b6ffb20247ca1adaf12bd45d8f7007efa7aec
x49x46 <fiyanov@yandex.ru>
2018-03-02 17:50:51 +0600

    Services

---
+++ app/services/StarService.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: x49x46
+ * Date: 02.03.2018
+ * Time: 17:28
+ */
+
+class StarService extends \Phalcon\DI\Injectable
+{
+    public function getList($filter = [])
+    {
+        return [];
+    }
+
+    public function getPersona($id)
+    {
+        return [];
+    }
+}
