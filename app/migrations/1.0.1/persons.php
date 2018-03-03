b95e172ec16dc84e4b50723bcdcf6c8dd0ea4733
x49x46 <fiyanov@yandex.ru>
2018-03-03 19:37:03 +0600

    database data

---
+++ app/migrations/1.0.1/persons.php
@@ -0,0 +1,149 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class PersonsMigration_101
+ */
+class PersonsMigration_101 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('persons', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'name',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 50,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'birthday',
+                        [
+                            'type' => Column::TYPE_DATE,
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'name'
+                        ]
+                    ),
+                    new Column(
+                        'birthplace',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 250,
+                            'after' => 'birthday'
+                        ]
+                    ),
+                    new Column(
+                        'country',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 50,
+                            'after' => 'birthplace'
+                        ]
+                    ),
+                    new Column(
+                        'career_status',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 50,
+                            'after' => 'country'
+                        ]
+                    ),
+                    new Column(
+                        'color_eye',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 50,
+                            'after' => 'career_status'
+                        ]
+                    ),
+                    new Column(
+                        'color_hair',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 50,
+                            'after' => 'color_eye'
+                        ]
+                    ),
+                    new Column(
+                        'height',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'size' => 11,
+                            'after' => 'color_hair'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY')
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '22',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8_general_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->batchInsert('persons', [
+                'id',
+                'name',
+                'birthday',
+                'birthplace',
+                'country',
+                'career_status',
+                'color_eye',
+                'color_hair',
+                'height'
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->batchDelete('persons');
+    }
+
+}
---
+++ app/migrations/1.0.1/social_pages.php
@@ -0,0 +1,90 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class SocialPagesMigration_101
+ */
+class SocialPagesMigration_101 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('social_pages', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'person_id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'size' => 11,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'url',
+                        [
+                            'type' => Column::TYPE_TEXT,
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'person_id'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY'),
+                    new Index('persona_id', ['person_id'], null)
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '5',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8_general_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->batchInsert('social_pages', [
+                'id',
+                'person_id',
+                'url'
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->batchDelete('social_pages');
+    }
+
+}
